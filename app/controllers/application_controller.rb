8b800f5475902360fa1afa8f70294068d4b2b9a0
hyuna-hub <hombo.yuna.04510@gmail.com>
2020-05-12 14:24:07 +0000

    [追加]ログイン〜マイページへの遷移、マイページのview

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+	protect_from_forgery with: :exception
+
+# サインアップ時にユーザー名を保存
+  	before_action :configure_permitted_parameters, if: :devise_controller?
+  	def configure_permitted_parameters
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  	end
+
+# ログイン・ログアウト後の遷移先
+	def after_sign_in_path_for(resource)
+    	user_path(current_user)
+  	end
+
+  	def after_sign_out_path_for(resource)
+    	root_path
+  	end
 end
