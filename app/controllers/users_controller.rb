62cc4a10055384a054f77db061e7e63dae7f7eda
hyuna-hub <hombo.yuna.04510@gmail.com>
2020-05-13 09:58:37 +0000

    [追加]ユーザーcontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,26 @@
 class UsersController < ApplicationController
 
 	def index
-		
+		@users = User.all
 	end
 
 	def show
-		
-	end
-
-	def new
-		
-	end
-
-	def create
-		
+		@user = User.find(params[:id])
+		@novels = @user.novels
 	end
 
 	def edit
-		
+		@user = User.find(params[:id])
 	end
 
 	def update
-		
+		@user = User.find(params[:id])
+    	if @user.update(user_params)
+    	   flash[:notice] = 'プロフィールを更新しました'
+      	   redirect_to user_path(@user)
+    	else
+      	   render :edit
+    	end
 	end
 
 	def destroy
